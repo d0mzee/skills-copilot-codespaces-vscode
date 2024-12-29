dc4ef7fdda8f009c73306f23768bc4347f97d9a6
d0mzee <80897139+d0mzee@users.noreply.github.com>
2024-12-29 11:47:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/member/member.html',
+        controller: 'MemberController',
+        controllerAs: 'memberCtrl',
+        bindToController: true,
+        scope: {
+            member: '='
+    }
+}
